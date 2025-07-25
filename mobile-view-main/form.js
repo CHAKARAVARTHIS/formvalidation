13d25306710924498f581fca2ea5c8db3d237106
CHAKARAVARTHIS <chakaravarthikumar@gmail.com>
2025-07-25 23:03:43 +0530

    first commit

---
+++ mobile-view-main/form.js
@@ -0,0 +1,20 @@
+let myform = document.getElementById('form');
+
+myform.addEventListener("submit", (event)=>{
+  event.preventDefault();
+  
+  let fullname = document.getElementById('name').value;
+  let mail = document.getElementById('email').value;
+  let password = document.getElementById("password").value;
+  let confirm = document.getElementById("confirm").value;
+  
+  if(!fullname || !mail || !confirm){
+    alert('please fill all feild');
+  }
+  
+  console.log(fullname);
+  console.log(mail);
+  console.log(confirm);
+  
+  myform.reset();
+});
